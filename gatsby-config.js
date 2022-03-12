d1cd2b7ae9e051f7a3dfdaa241c8b1eaba991539
Syed Faateh Sultan Kazmi <54814772+faatehsultan@users.noreply.github.com>
2022-03-12 19:00:28 +0500

    did

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Syed Faateh Sultan Kazmi - Portfolio Website`,
+    description: `Checkout my portfolio website.`,
+    author: `@faatehsultan`,
+    siteUrl: `https://faatehsultan.github.io/`,
   },
   plugins: [
     `gatsby-plugin-sass`,
